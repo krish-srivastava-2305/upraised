e2c243b747dd4b073dcdaf4e46d9709022c03d93
Krish Srivastava <krish.2022ug3018@iiitranchi.ac.in>
2025-06-10 20:37:19 +0530

    feat: add validation for login and signup routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { login, logout, signup } from "../controllers/user.controllers";
 import auth from "../middleware/auth.middleware";
+import { body } from "express-validator";
+import validateRequest from "../middleware/validator.middleware";
 
 const userRouter = Router();
 
-userRouter.post("/login", login);
-userRouter.post("/signup", signup);
+const credValidation = [
+    body("email").isEmail().withMessage("Invalid email format"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long")
+];
+
+userRouter.post("/login",
+    credValidation,
+    validateRequest,
+    login);
+userRouter.post("/signup", credValidation, validateRequest, signup);
 userRouter.post("/logout", auth, logout);
 
 export default userRouter;
